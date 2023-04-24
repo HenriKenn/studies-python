7a01a56481da0564ea27a72e125c8bf1836f10e3
HenriKenn <henriquesantino.cg@hotmail.com>
2023-04-24 20:31:07 -0300

    Calculadora de dobro e terça partes

---
+++ 07.py
@@ -0,0 +1,7 @@
+numb = float (input ("Digite um número: "))
+
+resultadodobro = (numb * 2)
+resultadoterça = (numb / 3)
+
+print (f"O dobro de {numb} é {resultadodobro}")
+print (f"A terça parte de {numb} é {resultadoterça}")
