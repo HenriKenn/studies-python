3f82b0a0097ef42af2bde8f39b38db0c2a8bebf3
HenriKenn <henriquesantino.cg@hotmail.com>
2023-04-24 20:15:42 -0300

    Calculadora de média de notas

---
+++ 05.py
@@ -0,0 +1,6 @@
+nota1 = float (input ("Digite a primeira nota: "))
+nota2 = float (input ("Digite a segunda nota: "))
+
+resultado = (nota1 + nota2)/2
+
+print (f"A média entre {nota1} e {nota2} igual a {resultado}")
