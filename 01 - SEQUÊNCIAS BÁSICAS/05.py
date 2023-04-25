b13fcb4b2fe841b4ecff2466f76b9600f780bddb
Felipe Henrique <henriquesantino.cg@hotmail.com>
2023-04-25 02:01:42 -0300

    Calculadora de nota média.

--- 01 - SEQUÊNCIAS BÁSICAS/05.py
+++ 01 - SEQUÊNCIAS BÁSICAS/05.py
@@ -1,4 +1,4 @@
-#Faça um programa que leia as duas notas de um aluno em uma matéria e mostre na tela a sua média na disciplina.
+# Faça um programa que leia as duas notas de um aluno em uma matéria e mostre na tela a sua média na disciplina.
 
 nota1 = float (input ("Digite a primeira nota: "))
 nota2 = float (input ("Digite a segunda nota: "))
