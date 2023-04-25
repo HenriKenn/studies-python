33fc78c391a3a6f10ad8d9709794d589fa824820
Felipe Henrique <henriquesantino.cg@hotmail.com>
2023-04-25 01:13:03 -0300

    Pergunta nome da pessoa e da boas vindas.

--- PASSO 01 - SEQUÊNCIAS BÁSICAS/02.py
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/02.py
@@ -1,4 +1,4 @@
 #leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela.
 
-nome = str (input("Qual é o seu nome? "))
+nome = str (input("Qual é o seu nome?: "))
 print (f"Olá {nome}, é um prazer te conhecer!")
