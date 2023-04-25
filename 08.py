ee9fb221f0cb0428de2b74f04b26edb8fb2621f7
HenriKenn <henriquesantino.cg@hotmail.com>
2023-04-24 21:25:32 -0300

    Calculadora Metros em km,hm,cm,mm,dm,dam

--- 08.py
+++ 08.py
@@ -1,1 +1,16 @@
-metros = float (input("Digite a distância em metros: "))
+print ("-=-"*20)
+metros = float(input("Digite a distância em metros: "))
+
+km = (metros / 1000)
+hm = (metros / 100)
+
+cm = (metros * 100)
+mm = (metros * 1000)
+
+dm = (metros * 10)
+dam = (metros / 10)
+
+print(f"A distância de {metros}m corresponde a:")
+
+print(f"{km} km\n {hm} hm \n {dam} dam \n {dm} dm \n {cm} cm \n {mm} mm")
+print ("-=-"*20)
