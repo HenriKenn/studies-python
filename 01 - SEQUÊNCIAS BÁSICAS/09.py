15f80ad13958f0db89d9fc99b78ea3aad0e36cfe
Felipe Henrique <henriquesantino.cg@hotmail.com>
2023-04-25 17:54:28 -0300

    Converso de reais em dóladres.

--- 01 - SEQUÊNCIAS BÁSICAS/09.py
+++ 01 - SEQUÊNCIAS BÁSICAS/09.py
@@ -1,4 +1,4 @@
-#Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$) e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.
+# Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$) e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.
 
 print ("-=-=-"*20)
 reais = float (input("--> Digite quanto você tem em R$ na sua carteira: "))
