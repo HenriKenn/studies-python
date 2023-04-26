0ba81f437465ea3b1e0566f10fea76fdc7f2a26f
Felipe Henrique <henriquesantino.cg@hotmail.com>
2023-04-26 18:22:19 -0300

    Calculadora de dias trabalho = salario.

---
+++ 01 - SEQUÊNCIAS BÁSICAS/15.py
@@ -0,0 +1,8 @@
+#Crie  um  programa  que  leia o  número  de  dias  trabalhados em  um  mês  e  mostre  o   salário  de um funcionário, sabendo que ele trabalha 8 horas por dia e  ganha R$25 por hora trabalhada.
+print("-=-="*20)
+
+dias = int(input("--> Digite o tanto de dias trabalhados: "))
+total = (25*8*dias)
+print(f"--> O sálario é de R${total}")
+
+print("-=-="*20)
