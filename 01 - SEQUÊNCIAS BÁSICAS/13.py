7f58a12abdf499a1877525eb574379d46fa42233
Felipe Henrique <henriquesantino.cg@hotmail.com>
2023-04-25 21:19:26 -0300

    Calculadora +15% Sálario.

--- 01 - SEQUÊNCIAS BÁSICAS/13.py
+++ 01 - SEQUÊNCIAS BÁSICAS/13.py
@@ -1,9 +1,9 @@
 #Faça um algoritmo que leia o salário de um funcionário, calcule e mostre o seu novo salário, com 15% de aumento.
 print ("-=-=-"*20)
 
-salario = float (input("Qual é o sálario do funcionario ? R$"))
+salario = float (input("--> Qual é o sálario do funcionario ? R$"))
 novo = salario + (salario * 15 / 100)
 
-print (f"Um funcionario que ganhava {salario:.2f} teve um aumento de 15% e passará a receber {novo:.2f}")
+print (f"--> Um funcionario que ganhava {salario:.2f} teve um aumento de 15% e passará a receber {novo:.2f}")
 
 print ("-=-=-"*20)
